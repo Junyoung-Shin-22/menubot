c9401a2ffd9e67623c5f693cbadec7fe5d3c09aa
Junyoung Shin <jun021222@naver.com>
2023-02-20 16:58:00 +0900

    Add heaven menu

--- menu.py
+++ menu.py
@@ -2,12 +2,15 @@ from collections import defaultdict
 
 MENU_LIST = defaultdict(lambda:MENU_LIST['기본'])
 
-with open('default.txt') as f:
-    MENU_LIST['기본'] = f.read().splitlines()
+def load_menu(file, key):
+    with open(file) as f:
+        MENU_LIST[key] = f.read().splitlines()
 
-with open('cup-ramen.txt') as f:
-    MENU_LIST['컵라면'] = f.read().splitlines()
+load_menu('default.txt', '기본')
+load_menu('cup-ramen.txt', '컵라면')
+load_menu('heaven.txt', '김밥천국')
 
+# unit tests
 if __name__ == '__main__':
     print(MENU_LIST['기본'])
     print(MENU_LIST['컵라면'])
